aa0e898ce82d1de096b5bdb96b9afd956ba0f6cb
Spooks <62370103+Spooks4576@users.noreply.github.com>
2024-03-21 22:28:36 -0600

    Update commandline.cpp

--- src/core/commandline.cpp
+++ src/core/commandline.cpp
@@ -362,7 +362,9 @@ void CommandLine::runCommand(String input)
           }
 
           DIALClient* dial = new DIALClient(Value.c_str(), SSID.c_str(), Password.c_str(), controller);
+#ifdef OLD_LED
           rgbmodule->setColor(LOW, HIGH, LOW);
+#endif
           dial->Execute();
           delete dial;
           delete controller;
