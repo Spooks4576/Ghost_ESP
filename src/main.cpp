10b3402b3871df6ea4119a34fef082d4859b180b
Spooks <62370103+Spooks4576@users.noreply.github.com>
2024-03-30 11:35:34 -0600

    Deauth Bypass Added

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,10 @@
 #include <Arduino.h>
 #include <SD.h>
 
+extern "C" int ieee80211_raw_frame_sanity_check(int32_t arg, int32_t arg2, int32_t arg3){
+  return 0;
+}
+
 void loop() {
 
 #ifdef DISPLAY_SUPPORT
