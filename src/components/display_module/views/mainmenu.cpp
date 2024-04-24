57c91576b49f408c080de9ffa0f312fe42854b54
Spooks <62370103+Spooks4576@users.noreply.github.com>
2024-04-24 14:49:15 -0600

    Finally Fixed Grid Container

--- src/components/display_module/views/mainmenu.cpp
+++ src/components/display_module/views/mainmenu.cpp
@@ -10,8 +10,8 @@ void MainMenu::Render()
 {
     lv_obj_clear_flag(lv_scr_act(), LV_OBJ_FLAG_SCROLLABLE);
     status_bar = create_status_bar(lv_scr_act());
-    grid_container = create_grid_container(lv_scr_act());
-    grid_label_container = create_grid_container(lv_scr_act());
+    grid_container = create_grid_container(lv_scr_act(), 0, 0);
+    lv_obj_clear_flag(grid_container, LV_OBJ_FLAG_SCROLLABLE);
     CreateGridButtons();
     Debugging = true;
 }
@@ -21,68 +21,55 @@ void MainMenu::CreateGridButtons()
     for (int i = 0; i < 3; i++) 
     {
         lv_obj_t *btn = nullptr;
-        if (i == 0)
-        {
-            btn = RenderImageToButton(grid_container, &bt_img, 45, 0, 0, 50, 50);
-            OtherObjects.add(btn);
-            lv_obj_t *label = lv_label_create(grid_label_container); 
-            lv_label_set_text(label, "BT");
-            static lv_style_t label_text_style;
-            lv_style_init(&label_text_style);
-            lv_style_set_text_font(&label_text_style, &Juma);
-            lv_style_set_text_color(&label_text_style, lv_color_white());
-            lv_obj_add_style(label, &label_text_style, 0);
-            lv_obj_set_style_transform_angle(label, 900, 0);
-            lv_obj_set_style_transform_pivot_x(label, lv_obj_get_width(label) / 2, 0);
-            lv_obj_set_style_transform_pivot_y(label, lv_obj_get_height(label) / 2, 0);
-        }
 
-        if (i == 1)
-        {
-            btn = RenderImageToButton(grid_container, &WiFi_img, 45, 0, 0, 50, 50);
-            OtherObjects.add(btn);
-            lv_obj_t *label = lv_label_create(grid_label_container); 
-            lv_label_set_text(label, "WiFi");
-            static lv_style_t label_text_style;
-            lv_style_init(&label_text_style);
-            lv_style_set_text_font(&label_text_style, &Juma);
-            lv_style_set_text_color(&label_text_style, lv_color_white());
-            lv_obj_add_style(label, &label_text_style, 0);
-            lv_obj_set_style_transform_angle(label, 900, 0);
-            lv_obj_set_style_transform_pivot_x(label, lv_obj_get_width(label) / 2, 0);
-            lv_obj_set_style_transform_pivot_y(label, lv_obj_get_height(label) / 2, 0);
-        }
+        lv_obj_t *btn_container = lv_obj_create(grid_container); 
+        lv_obj_set_size(btn_container, 70, 70); 
 
-        if (i == 2)
-        {
-            btn = RenderImageToButton(grid_container, &led_img, 45, 0, 0, 50, 50);
-            OtherObjects.add(btn);
-            lv_obj_t *label = lv_label_create(grid_label_container);
-            lv_label_set_text(label, "LED");
-            static lv_style_t label_text_style;
-            lv_style_init(&label_text_style);
-            lv_style_set_text_font(&label_text_style, &Juma);
-            lv_style_set_text_color(&label_text_style, lv_color_white());
-            lv_obj_add_style(label, &label_text_style, 0);
-            lv_obj_set_style_transform_angle(label, 900, 0);
-            lv_obj_set_style_transform_pivot_x(label, lv_obj_get_width(label) / 2, 0);
-            lv_obj_set_style_transform_pivot_y(label, lv_obj_get_height(label) / 2, 0);
-        }
+        switch (i) {
+            case 0:  btn = RenderImageToButton(btn_container, &bt_img, 45, 55, -5, 50, 50); break;
+            case 1:  btn = RenderImageToButton(btn_container, &WiFi_img, 45, 55, -5, 50, 50); break;
+            case 2:  btn = RenderImageToButton(btn_container, &led_img, 45, 55, -5, 50, 50); break;
+        } 
 
         if (btn)
         {
+            OtherObjects.add(btn_container); // For Touch Controls
             lv_obj_set_scrollbar_mode(btn, LV_SCROLLBAR_MODE_OFF);
             lv_obj_set_style_transform_angle(btn, 900, 0);
             lv_obj_set_style_transform_pivot_x(btn, lv_obj_get_width(btn) / 2, 0);
             lv_obj_set_style_transform_pivot_y(btn, lv_obj_get_height(btn) / 2, 0);
+            lv_obj_set_scrollbar_mode(btn, LV_SCROLLBAR_MODE_OFF);
+            lv_obj_set_scrollbar_mode(btn_container, LV_SCROLLBAR_MODE_OFF);
+            lv_obj_clear_flag(btn_container, LV_OBJ_FLAG_SCROLLABLE);
 
             static lv_style_t style_btn;
             lv_style_init(&style_btn);
             lv_style_set_bg_color(&style_btn, lv_color_hex(0x000));
             lv_style_set_bg_opa(&style_btn, LV_OPA_COVER);
             lv_style_set_radius(&style_btn, 10);
+            lv_style_set_border_width(&style_btn, 0);
             lv_obj_add_style(btn, &style_btn, 0);
+            lv_obj_add_style(btn_container, &style_btn, 0);
         }
+
+        lv_obj_t *label = lv_label_create(btn_container); 
+        lv_obj_align(label, LV_ALIGN_OUT_BOTTOM_MID, 0, 5);
+
+        switch (i) {
+            case 0:  lv_label_set_text(label, "BLE"); break;
+            case 1:  lv_label_set_text(label, "WiFi"); break;
+            case 2:  lv_label_set_text(label, "LED"); break;
+        } 
+
+        static lv_style_t label_text_style;
+        lv_style_init(&label_text_style);
+        lv_style_set_text_font(&label_text_style, &Juma);
+        lv_style_set_text_color(&label_text_style, lv_color_white());
+        lv_obj_add_style(label, &label_text_style, 0); 
+        lv_obj_set_style_transform_angle(label, 900, 0);
+        lv_obj_set_scrollbar_mode(label, LV_SCROLLBAR_MODE_OFF);
+        lv_obj_set_style_transform_pivot_x(label, lv_obj_get_width(label) / 2, 0);
+        lv_obj_set_style_transform_pivot_y(label, lv_obj_get_height(label) / 2, 0);
     }
 }
 
@@ -91,7 +78,7 @@ void MainMenu::UpdateWifiChannelStatus(int Channel)
     lv_label_set_text(WifiChannelLabel, String("CH: " + Channel).c_str());
 }
 
-lv_obj_t* MainMenu::create_grid_container(lv_obj_t * parent)
+lv_obj_t* MainMenu::create_grid_container(lv_obj_t *parent, int x, int y) 
 {
     lv_obj_t *lgrid_container = lv_obj_create(lv_scr_act());
     lv_obj_set_size(lgrid_container, LV_PCT(100), LV_PCT(100));
@@ -99,17 +86,16 @@ lv_obj_t* MainMenu::create_grid_container(lv_obj_t * parent)
     lv_obj_set_flex_flow(lgrid_container, LV_FLEX_FLOW_COLUMN_WRAP);
     lv_obj_set_style_border_width(lgrid_container, 0, LV_PART_MAIN);
     lv_obj_set_style_transform_pivot_x(lgrid_container, lv_obj_get_width(lgrid_container) / 2, 0);
-    lv_obj_set_style_transform_pivot_y(lgrid_container, lv_obj_get_height(lgrid_container) / 2, 0);   
+    lv_obj_set_style_transform_pivot_y(lgrid_container, lv_obj_get_height(lgrid_container) / 2, 0); 
     lv_obj_set_style_outline_width(lgrid_container, 0, LV_PART_MAIN);
     lv_obj_set_flex_align(lgrid_container, LV_FLEX_ALIGN_SPACE_EVENLY, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
-    lv_obj_set_pos(lgrid_container, 50, 0);
+    lv_obj_set_pos(lgrid_container, x, y);
     static lv_style_t style_grid;
     lv_style_init(&style_grid);
     lv_style_set_bg_opa(&style_grid, LV_OPA_TRANSP);
-    lv_style_set_grid_column_dsc_array(&style_grid, grid_col_dsc);
-    lv_style_set_grid_row_dsc_array(&style_grid, grid_row_dsc);
+    lv_style_set_grid_column_dsc_array(&style_grid, grid_col_dsc); // Assumes you have grid_col_dsc defined
+    lv_style_set_grid_row_dsc_array(&style_grid, grid_row_dsc); // Assumes you have grid_row_dsc defined
     lv_style_set_layout(&style_grid, LV_LAYOUT_GRID);
-
     lv_obj_add_style(lgrid_container, &style_grid, 0);
     return lgrid_container;
 }
