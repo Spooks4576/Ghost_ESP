ec8b9b45eefdd3abd3dd3d5d0a4e6ae969b61d91
Spooks <62370103+Spooks4576@users.noreply.github.com>
2024-11-08 00:07:32 -0700

    Update MicroNMEA.h

--- include/vendor/GPS/MicroNMEA.h
+++ include/vendor/GPS/MicroNMEA.h
@@ -161,7 +161,7 @@ typedef void *nmea_parser_handle_t;
     {                                             \
         .uart = {                                 \
             .uart_port = UART_NUM_1,              \
-            .rx_pin = 1,\
+            .rx_pin = 1,                        \
             .baud_rate = 9600,                    \
             .data_bits = UART_DATA_8_BITS,        \
             .parity = UART_PARITY_DISABLE,        \
